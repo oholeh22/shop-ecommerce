f325fa2030e79b12b0bb50c43303f904705df8c3
Oleh Shyian <shiyan4242@gmail.com>
2025-05-09 18:48:09 +0200

    Add items + css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+// src/App.js
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Items  from './components/Items';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { id: 1, title: 'Стул серый',         img: 'chair-grey.jpg',      desc: 'Удобный современный стул с мягкой обивкой в сером цвете.',         category: 'chairs',  price: '49.99' },
+        { id: 2, title: 'Журнальный столик',   img: 'table.jpg',desc: 'Компактный журнальный столик из натурального дерева.',              category: 'tables',  price: '79.50' },
+        { id: 3, title: 'Настольная лампа',    img: 'lamp.jpg',  desc: 'Регулируемая настольная лампа в черном матовом корпусе.',          category: 'lighting',price: '29.90' },
+        { id: 4, title: 'Диван двухместный',   img: 'sofa.jpg',        desc: 'Комфортный диван на двоих с прочным каркасом и синим велюром.',      category: 'sofas',   price: '299.00' },
+        { id: 5, title: 'Письменный стол',     img: 'office-table.jpg',desc: 'Минималистичный белый письменный стол с ящиком для хранения.',     category: 'desks',   price: '159.99' },
+        { id: 6, title: 'Книжная полка',       img: 'book-shelf.jpg',   desc: 'Высокая открытая книжная полка из бука для вашего интерьера.',     category: 'shelves', price: '199.75' }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Items.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Item from './Item';
+
+export default class Items extends Component {
+    render() {
+      return (
+        <main>
+          {this.props.items.map(el => (
+            <Item key={el.id} item={el}/>
+          ))}
+        </main>
+      );
+    }
+  }
